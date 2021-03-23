e8530624ffdf336edbb4c7a226338ba70907b7dc
Erich Sauer <erich.sauer@gmail.com>
2021-03-22 17:32:52 -0700

    removes .push array method use from original function

--- capitalize-and-filter.js
+++ capitalize-and-filter.js
@@ -1,8 +1,17 @@
 const capitalizeAndFilter = (array, filterBy) => {
-  const result = array
-    .filter((element) => element.charAt(0) !== filterBy)
-    .map((element) => element.toUpperCase());
+  let result = [];
+  for (const item of array) {
+    if (item.charAt(0) !== filterBy) {
+      result = [...result, item.toUpperCase()];
+    }
+  }
   return result;
 };
+// const capitalizeAndFilter = (array, filterBy) => {
+//   const result = array
+//     .filter((element) => element.charAt(0) !== filterBy)
+//     .map((element) => element.toUpperCase());
+//   return result;
+// };
 
 module.exports = capitalizeAndFilter;
